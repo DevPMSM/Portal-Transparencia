4a58fe9ccd43b6ca6df2b21d844b52bc678f9b77
Gustavo Schattner <gucschattner@gmail.com>
2024-03-18 17:29:13 -0300

    chore: Atualização das variáveis

--- manage.py
+++ manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'portal_transparencia.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'index_main.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
